7baa5d2387c712d8933e4ff80a46bda504ffb277
Matheus Medrado <matheus-13-@hotmail.com>
2022-08-03 20:18:37 -0300

    adicionando arquivos

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', context={'name': 'Matheus medrado',})
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+def sobre(request):
+    return render(request, 'recipes/sobre.html')
