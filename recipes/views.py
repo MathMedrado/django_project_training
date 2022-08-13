43bed77fb9ab9479408f17679a0f78b75bf86718
Matheus Medrado <matheus-13-@hotmail.com>
2022-08-12 21:56:26 -0300

    models adicionados

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from recipes.models import Recipe
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)],})
+    recipes = Recipe.objects.all().filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes,})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(),'is_detail_page':True,})
+    recipes = Recipe.objects.filter(id=id, is_published=True).first()
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': recipes,'is_detail_page':True,})
 
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={'recipes': recipes,})
